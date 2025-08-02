2095efbfc8b4f2f24a2567201b65064685ae5958
anuragmkmp <141000918+anuragmkmp@users.noreply.github.com>
2025-08-02 19:55:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
+  res.write('<center>');
+  res.write('<h1>Hello World!</h1>');
+  res.write('<h1>Welcome to First NodeJS</h1>');
+  res.write('</center>');
   res.end();
+
 }).listen(8080);
